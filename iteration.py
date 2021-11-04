65a89a984366f68ab71b20418194974d3efd51e3
eric <ericscorpio1@gmail.com>
2021-11-03 20:59:13 -0700

    init_new _files

---
+++ iteration.py
@@ -0,0 +1,14 @@
+# Loop over a list
+for n in range(1,10):
+    print(n)
+
+for n in range(10, -1, -1):
+    print(n)
+
+for n in range(2,50,3):
+    print(n)
+
+player = ['jason', 'bill', 'tommy', 'derrick']
+
+for n in player:
+    print(n)
